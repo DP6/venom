c5a9061bb2d49f94ef9ca57bd5f1a40288d66b8d
Paulo Brumatti <paulo.brumatti@dp6.com.br>
2014-12-11 19:33:59 -0200

    Adicionado throw e pequenas alterações no timeOnPage.js

--- plugins/timeOnPage.js
+++ plugins/timeOnPage.js
@@ -3,8 +3,14 @@
 
 	function timeOnPage(util, opt_config) {
 		opt_config = opt_config || {};
+
+		if (opt_config.timeBucket && util.typeOf(opt_config.timeBucket) !== 'Array')
+			throw util.errorBuilder('venom:plugin[timeOnPage]', '"handler" is not an array');
+		if (opt_config.category && typeof opt_config.category !== 'string')
+			throw util.errorBuilder('venom:plugin[timeOnPage]', '"category" is not a string');
+
 		opt_config.timeBucket = opt_config.timeBucket || [1, 2, 5, 10, 20, 30, 40, 50, 60, 120, 180, 300, 600, 900, 1200, 1500];
-		opt_config.category = opt_config.category || 'TimeOnPage (s)';
+		opt_config.category = opt_config.category || 'Time on Page(s)';
 		var performance = window.performance;
 		var start;
 
@@ -13,15 +19,15 @@
 		}
 
 		util.addListener(window, 'unload', function () {
-			var time = start ? (new Date().getTime() - start) : performance.now();
-			var range = findInBucket(time / 1000, opt_config.bucket);
+			var ms = start ? (new Date().getTime() - start) : performance.now();
+			var range = getRange(ms / 1000, opt_config.bucket);
 			ga('send', 'event', opt_config.category, range, range);
 		});
 	}
 
-	function findInBucket(time, timeBucket) {
+	function getRange(ms, timeBucket) {
 		for (var i = 0, length = timeBucket.length; i < length; i++) {
-			if (timeBucket[i] > time) break;
+			if (timeBucket[i] > ms) break;
 		}
 
 		return timeBucket[i + 1] ? timeBucket[i] + '-' + timeBucket[i + 1] : timeBucket[i - 1] + '+';
