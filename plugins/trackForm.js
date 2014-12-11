cabcd0038e2a9c000161a1e7ebaeee907cf312b2
Paulo Brumatti <paulo.brumatti@dp6.com.br>
2014-12-11 19:55:04 -0200

    Adicionado throw e pequenas alterações no trackForm.js

--- plugins/trackForm.js
+++ plugins/trackForm.js
@@ -9,54 +9,64 @@
 		this.venomFormTracked = true;
 
 		if (opt_config.handler && typeof opt_config.handler !== 'function')
-			throw util.errorBuilder('plugin:trackForm', '"handler" is not a function');
+			throw util.errorBuilder('venom:plugin[trackForm]', '"handler" is not a function');
 		if (opt_config.category && typeof opt_config.category !== 'string')
-			throw util.errorBuilder('plugin:trackForm', '"category" is not a string');
+			throw util.errorBuilder('venom:plugin[trackForm]', '"category" is not a string');
 
-		opt_config.category = opt_config.category || 'Form Tracking';
+		if (!opt_config.category)
+			opt_config.category = 'Form Tracking';
+
+		if (!opt_config.handler)
+			opt_config.handler = function (info) {
+				var action = 'form (' + info.formName + ')';
+				var label = info.elementName + ' (' + info.event.type + ')';
+				ga('send', 'event', opt_config.category, action, label);
+			};
 
 		function trackField(event) {
-			var element = event.target,
-				elementName = element.name || element.id || element.type || element.nodeName,
-				formName = getFormName(element),
-				action = 'form (' + formName + ')',
-				label = elementName + ' (' + event.type + ')';
+			event = event || window.event;
+			var element = event.target;
+			var formName = getFormName(element);
+			var elementName = getAttribute(element, 'name') ||
+				getAttribute(element, 'id') ||
+				getAttribute(element, 'type') ||
+				element.nodeName;
 
-			if (typeof opt_config.handler === 'function') {
+			try {
 				opt_config.handler({
 					element: element,
 					elementName: elementName,
 					formName: formName,
 					event: event
 				});
-			} else {
-				ga('send', 'event', opt_config.category, action, label);
+			} catch (e) {
+				throw util.errorBuilder('venom:plugin[trackForm]', 'opt_config.handler:' + e);
 			}
 		}
 
 		function getFormName(element) {
 			while (element && element.nodeName !== 'HTML') {
 				if (element.nodeName === 'FORM') {
-					return element.name || element.id || 'none';
+					return getAttribute(element, 'name') || getAttribute(element, 'id') || 'none';
 				}
 				element = element.parentNode;
 			}
 			return 'none';
 		}
 
-		util.domReady(function () {
-			var changeTags = ['input', 'select', 'textarea', 'hidden'],
-				forms = document.getElementsByTagName('form'),
-				elements;
+		function getAttribute(node, attr) {
+			return typeof node[attr] === 'string' ? node[attr] : node.getAttribute(attr);
+		}
 
-			util.forEach(changeTags, function (tagName) {
-				elements = document.getElementsByTagName(tagName);
+		util.domReady(function () {
+			util.forEach(['input', 'select', 'textarea', 'hidden'], function (tagName) {
+				var elements = document.getElementsByTagName(tagName);
 				util.forEach(elements, function (element) {
 					util.addListener(element, 'change', trackField);
 				});
 			});
 
-			util.forEach(forms, function (form) {
+			util.forEach(document.getElementsByTagName('form'), function (form) {
 				util.addListener(form, 'submit', trackField);
 			});
 		});
