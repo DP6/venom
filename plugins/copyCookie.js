b720fe2da94e54f7d4bcccf31dffea300fcd017a
Paulo Brumatti <paulo.brumatti@dp6.com.br>
2014-12-11 20:39:16 -0200

    Adicionado throw e pequenas alterações no copyCookie.js

--- plugins/copyCookie.js
+++ plugins/copyCookie.js
@@ -2,34 +2,38 @@
 	var ga = window[window.GoogleAnalyticsObject || 'ga'];
 
 	function copyCookie(util, config) {
-		var elementLinks;
-		if (!config.listDomain)
-			throw util.errorBuilder('plugin:copyCookie', '"listDomain" was not supplied');
+		if (!config || !config.domainList)
+			throw util.errorBuilder('plugin:copyCookie', '"domainList" was not supplied');
+		if (util.typeOf(config.domainList) !== 'Array')
+			throw util.errorBuilder('plugin:copyCookie', '"domainList" is not an array');
 
-		this.listDomain = config.listDomain;
-		this.params = config.params || [util.getParamURL()];
-
-		function decorateMe(event) {
-			event = event || window.event;
-			var target = event.target || event.srcElement;
-
-			if (target && target.href) {
-				ga('linker:decorate', target);
-			}
-		};
-
-		elementLinks = document.getElementsByTagName('a');
-		for (var i = elementLinks.length - 1; i >= 0; i--) {
-			util.addListener(elementLinks[i], 'mousedown', function () {
-				decorateMe(event);
-			});
-		}
-
-		VenomCopyCookie.prototype.set = function () {
+		if (config.useAutoLinker) {
 			// Load plugin to listener all links on the page and automatically fire cookie GA.
 			ga('require', 'linker');
-			ga('linker:autoLink', this.listDomain);
-		};
+			ga('linker:autoLink', config.domainList);
+		} else {
+			util.forEach(document.getElementsByTagName('a'), function (el) {
+				util.addListener(el, 'mousedown', function () {
+					if (verifyDomain(util, config.domainList, this.hostname)) {
+						event = event || window.event;
+						var target = event.target || event.srcElement;
+
+						if (target && target.href) {
+							ga('linker:decorate', target);
+						}
+					}
+				});
+			});
+		}
+	}
+
+	function verifyDomain(util, domainList, host) {
+		var ret = false;
+		util.forEach(domainList, function (domain) {
+			if (typeof domain === 'string' && host.indexOf('domain') >= 0 || util.typeOf(domain) === 'RegExp' && domain.test(host))
+				ret = true;
+		});
+		return ret;
 	}
 
 	if (ga)
