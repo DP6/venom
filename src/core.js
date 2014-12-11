04dadd741bdd4b0a2ddcd92b9d74f3c9bc73a63b
Bruna Lima <brulima>
2014-12-11 20:29:08 -0200

    Update Core
    Trigger de hits

--- src/core.js
+++ src/core.js
@@ -138,6 +138,44 @@ function trigger(eventName, opt_data) {
 }
 
 // ====================
+// Name: 
+// Description: 
+// ====================o3s
+
+ga(function(tracker) {
+	var task = tracker.get('sendHitTask');
+
+	tracker.set('sendHitTask', function(data) {
+		var hitType = data.get('hitType');
+
+		if (hitType === 'pageview') {
+			ga('venom:trigger', 'gaPageview', {
+				page: data.get('page') || document.location.pathname,
+				title: data.get('title') || document.title
+			});
+		} else if (hitType === 'event') {
+			ga('venom:trigger', 'gaEvent', {
+				eventCategory: data.get('eventCategory'),
+				eventAction: data.get('eventAction'),
+				eventLabel: data.get('eventLabel'),
+				eventValue: data.get('eventValue'),
+				page: data.get('page') || document.location.pathname
+			});
+		} else if (hitType === 'timing') {
+			ga('venom:trigger', 'gaPageview', {
+				timingCategory: data.get('timingCategory'),
+				timingVar: data.get('timingVar'),
+				timingValue: data.get('timingValue'),
+				timingLabel: data.get('timingLabel'),
+				page: data.get('page') || document.location.pathname
+			});
+		}
+
+		task(data);
+	});
+});
+
+// ====================
 // Name: Util
 // Description: Useful methods
 // ====================
